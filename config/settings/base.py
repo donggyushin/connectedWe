e64bab10c11f34a4f60ccafb6edbd94d527e4015
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-07-23 18:20:55 +0900

    Build a Image application

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'connectedwe.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'connectedwe.images.apps.ImagesConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
