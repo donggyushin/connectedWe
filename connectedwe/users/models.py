d2064bdbf7a7daa774bf9645b47593e56666c6a2
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-07-24 15:11:02 +0900

    Edit of user models

--- connectedwe/users/models.py
+++ connectedwe/users/models.py
@@ -2,13 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
+    
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not Specified')
+    )
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(_("webiste"), max_length=200, null=True)
+    bio = models.TextField(_("bio"), null=True)
+    phone = PhoneNumberField(_("phone"), null=True)
+    gender = models.CharField(_("gender"), max_length=50, choices=GENDER_CHOICES, null=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
